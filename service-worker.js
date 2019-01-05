ee15597f4aca06c5b399f52d4e19a7061e8002d0
Sathya <sathyahari9@gmail.com>
2019-01-06 01:19:59 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/sathyahari.me/precache-manifest.4b28f033193c46698434f82ce385e32c.js"
+  "/sathyahari.me/precache-manifest.6aa2ecd8d2ccb3163b37c1ec817da4e2.js"
 );
 
 workbox.clientsClaim();

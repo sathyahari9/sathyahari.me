0ada5709491722d06260fbe30f04b5f5cfbd4537
Sathya <sathyahari9@gmail.com>
2019-01-06 00:48:10 +0530

    changes

---
+++ build/precache-manifest.2dd972c3518bde75a45c823adfe19683.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a692b6ed7ff35235c4b79f1bc9f39210",
+    "url": "/sathyahari.me/static/media/s.a692b6ed.png"
+  },
+  {
+    "revision": "a1ff2fd4f10072221fa7fd44f9f08196",
+    "url": "/sathyahari.me/static/media/resume.a1ff2fd4.pdf"
+  },
+  {
+    "revision": "e2c89903583c9e07c8ad58787e3307a8",
+    "url": "/sathyahari.me/static/media/logoshadow.e2c89903.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sathyahari.me/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2d382826d1288b695d5a",
+    "url": "/sathyahari.me/static/js/runtime~main.2d382826.js"
+  },
+  {
+    "revision": "e9cb0a3874ab1d2513b7",
+    "url": "/sathyahari.me/static/js/main.e9cb0a38.chunk.js"
+  },
+  {
+    "revision": "6316728995d3e3cf0cbd",
+    "url": "/sathyahari.me/static/js/1.63167289.chunk.js"
+  },
+  {
+    "revision": "e9cb0a3874ab1d2513b7",
+    "url": "/sathyahari.me/static/css/main.141299a3.chunk.css"
+  },
+  {
+    "revision": "98904346bd99c93529d01dfb1de54a19",
+    "url": "/sathyahari.me/index.html"
+  }
+];
